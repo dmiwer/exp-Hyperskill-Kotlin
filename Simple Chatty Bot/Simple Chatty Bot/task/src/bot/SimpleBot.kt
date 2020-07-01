1d787df7301c6e01697f0939cc5c679a5cb449d4
dmiwer <dmiwer@gmail.com>
2020-07-01 20:11:30 +0300

    Simple Chatty Bot 4/4

--- Simple Chatty Bot/Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -3,30 +3,60 @@ package bot
 import java.util.*
 
 
+val scanner = Scanner(System.`in`) // Do not change this line
+
 fun main() {
-    val scanner = Scanner(System.`in`)
+    greet("Aid", "2020") // change it as you need
+    remindName()
+    guessAge()
+    count()
+    // ...
+    test()
+    end()
+}
 
-    println("Hello! My name is Aid.")
-    println("I was created in 2020.")
+fun greet(assistantName: String, birthYear: String) {
+    println("Hello! My name is " + assistantName + ".")
+    println("I was created in " + birthYear + ".")
     println("Please, remind me your name.")
+}
 
+fun remindName() {
     val name = scanner.nextLine()
-
     println("What a great name you have, " + name + "!")
-    println("Let me guess your age.")
-    println("Enter remainders of dividing your age by 3, 5 and 7.")
+}
 
+fun guessAge() {
+    println("Let me guess your age.")
+    println("Say me remainders of dividing your age by 3, 5 and 7.")
     val rem3 = scanner.nextInt()
     val rem5 = scanner.nextInt()
     val rem7 = scanner.nextInt()
     val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-
     println("Your age is " + age + "; that's a good time to start programming!")
+}
+
+fun count() {
     println("Now I will prove to you that I can count to any number you want.")
+    val num = scanner.nextInt()
+    for (i in 0..num) {
+        print(i)
+        println("!")
+    }
+}
 
-    // read a number and count to it here
-    for(i in 0 .. scanner.nextInt())
-        println("$i!")
+fun test() {
+    println("Let's test your programming knowledge.")
+    // write your code here
+    print("Why do we use methods?\n" +
+            "1. To repeat a statement multiple times.\n" +
+            "2. To decompose a program into several small subroutines.\n" +
+            "3. To determine the execution time of a program.\n" +
+            "4. To interrupt the execution of a program.\n")
+    while (scanner.nextInt() != 2)
+        println("Please, try again.")
+}
 
-    println("Completed, have a nice day!")
+fun end() {
+    println("Congratulations, have a nice day!") // Do not change this text
 }

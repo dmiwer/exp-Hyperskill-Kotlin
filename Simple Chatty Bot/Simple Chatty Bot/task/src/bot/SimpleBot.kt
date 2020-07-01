c5ecfec98715337563944bf546dafd360d4313c1
dmiwer <dmiwer@gmail.com>
2020-07-01 05:17:56 +0300

    Simple Chatty Bot 2/4

--- Simple Chatty Bot/Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -1,7 +1,27 @@
 package bot
 
+import java.util.*
+
+
 fun main() {
-    // write your code here
+    val scanner = Scanner(System.`in`)
+
     println("Hello! My name is Aid.")
     println("I was created in 2020.")
+    println("Please, remind me your name.")
+
+    // reading a name
+    val name = scanner.next()
+
+    println("What a great name you have, $name!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    // reading all remainders
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+    val c = scanner.nextInt()
+    val age = (a * 70 + b * 21 + c * 15) % 105
+
+    println("Your age is $age; that's a good time to start programming!")
 }
